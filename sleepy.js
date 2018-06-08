f5ea603c0d41a16c0112e8e5fc8e9614fd12a099
Isaac Avram <avrisaac555@gmail.com>
2018-06-08 11:43:23 -0700

    Finalized things

--- sleepy.js
+++ sleepy.js
@@ -1,12 +1,14 @@
 const express = require("express")
 
 const app = express()
-app.set('view engine', 'pug')
+app.set("view engine", "pug")
 
 app.get("/", function(req, res) {
-    res.render("index")
+    res.render("index", {"title": "Turn my computer off!"})
 });
 
-app.post("/sleep/:time(\d+)", function(req, res) {
-
+app.post("/sleep", function(req, res) {
+    res.render("sleep", {"title": "Success!"})
 });
+
+app.listen(9000)
