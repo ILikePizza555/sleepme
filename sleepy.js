9e0d729a898f271184a3aa25d7aac44538ccc64b
Isaac Avram <avrisaac555@gmail.com>
2018-06-08 11:57:35 -0700

    Added snark

--- sleepy.js
+++ sleepy.js
@@ -1,5 +1,20 @@
 const express = require("express")
 
+const snark = [
+    "Wow thanks, I was using that.",
+    "A $100 usage fee has automatically been charged to your nearest checking account.",
+    "Next time format your request using JSON",
+    "Brought to you by Depression(tm), Insanity(tm), and Psychopathy(tm).",
+    "Tip: You can tell it works if reloading the page fails.",
+    "Why did I do this again?",
+    "Was it really worth it?",
+    "You realize I'm just going to turn back on again right?",
+    "You just helped save a total of 1.45 μWh of electricity!",
+    "I really appreciate it.",
+    "I read all of your messages. Not by choice.",
+    "I wonder if I should make a Twitch stream"
+]
+
 const app = express()
 app.set("view engine", "pug")
 
@@ -8,7 +23,7 @@ app.get("/", function(req, res) {
 });
 
 app.post("/sleep", function(req, res) {
-    res.render("sleep", {"title": "Success!"})
+    res.render("sleep", {"title": "Success!", "snark": snark[Math.floor(Math.random() * snark.length)]});
 });
 
 app.listen(9000)
